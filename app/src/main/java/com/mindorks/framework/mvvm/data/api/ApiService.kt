7ed591f229a3343bc3202927215978d51e2f8a3f
oystrr <oyesteeer@gmail.com>
2021-10-26 09:11:02 +1100

    Initial commit

---
+++ app/src/main/java/com/mindorks/framework/mvvm/data/api/ApiService.kt
@@ -0,0 +1,7 @@
+package com.mindorks.framework.mvvm.data.api
+import com.mindorks.framework.mvvm.data.model.User
+import io.reactivex.Single
+
+interface ApiService {
+    fun getUsers(): Single<List<User>>
+}
